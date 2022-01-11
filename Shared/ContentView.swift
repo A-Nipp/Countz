a88bd3aacbd02a65b03c13977bf19fb7b8f29493
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-01-11 18:54:14 -0500

    Initial commit. Working counter and visual changes.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0
+    let max = 10
+    var progress: Double {
+        Double(count) / Double(max)
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [.blue, .white], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                LinearGradient(colors: [.red,.white], startPoint: .top, endPoint: .bottom)
+                    .opacity(progress)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("\(count)")
+                        .font(.largeTitle)
+                    Spacer()
+                    HStack{
+                        Button(action: { count -= 1}, label: { Image(systemName: "minus")})
+                        Spacer()
+                        Button(action: { count += 1}, label: { Image(systemName: "plus")})
+                    }
+                    .padding()
+                }
+                .padding()
+                .navigationTitle("Countz")
+                //.navigationBarHidden(true)
+                
+                
+            }
+        }
     }
 }
 
